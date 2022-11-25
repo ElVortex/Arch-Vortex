0352a00c0ad12ca4bbecbbf2a5afc0d2274a9832
ElVortex <paulo.g.mamani@gmail.com>
2022-11-24 21:40:32 -0300

    Update scripts.sh

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -10,8 +10,10 @@ package_list () {
       packages="$packages $line"
       if [[ ${INSTALL_TYPE} == 'FULL' ]]; then
         packages=`echo $packages | sed 's/ \-\-END OF MINIMAL INSTALL\-\- / /g'`
+        echo 'Installing Full list'
       elif [[ ${INSTALL_TYPE} == 'MINIMAL' ]]; then
         packages=`echo $packages | sed 's/ \-\-END OF MINIMAL INSTALL.*/ /g'`
+        echo 'Installing Partial list'
       fi
     done < $1
 }
